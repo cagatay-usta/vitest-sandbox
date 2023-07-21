3122b7420d30a5dcd703a18c17d9cf2393f729bf
cagatay-usta <cagatay_usta@windowslive.com>
2023-07-22 00:34:09 +0300

    experiment with user events

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 
 function App() {
+  const [heading, setHeading] = useState("Magnificent Monkeys")
 
+  function clickHandler() {
+    setHeading("Radical Rhinos")
+  }
   return (
     <>
+    <button type="button" onClick={clickHandler}> Click Me</button>
+    <h1>{heading}</h1>
     </>
   )
 }
